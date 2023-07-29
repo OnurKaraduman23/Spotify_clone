c4148a66fc025c7c8aa711ea2fe32c3836fef27b
Honor13 <onury.karaduman@gmail.com>
2023-07-29 21:30:38 +0300

    Spotify (Android Studio) 15. Update

---
+++ app/src/main/java/com/example/spotify/Anasayfa2CardviewVerileri.kt
@@ -0,0 +1,6 @@
+package com.example.spotify
+
+import java.io.Serializable
+
+data class Anasayfa2CardviewVerileri(var veri2_id:Int, var veri2_baslik:String, var veri2_metin:String, var veri2_gorsel:String):Serializable {
+}
---
+++ app/src/main/java/com/example/spotify/AnasayfaVeri2Adapter.kt
@@ -0,0 +1,21 @@
+package com.example.spotify
+
+import android.content.Context
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class AnasayfaVeri2Adapter(private val mContext:Context, private val AnasayfaVeri2Verileri:List<Anasayfa2CardviewVerileri>) {
+//
+//    inner class CardViewNesneTutucu(view:View):RecyclerView.ViewHolder(view){
+//
+//        var imageView2Gorsel:ImageView
+//        var txtMtn2:TextView
+//        var textViewBaslik:TextView
+//
+//        init {
+//            imageView2Gorsel = view.findViewById(R.id.imageView2Gorsel)
+//        }
+//    }
+}
